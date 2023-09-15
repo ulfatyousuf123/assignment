69dafe2495d67ea6246a2e8b18836c35ee4b1673
ulfatyousuf <snow10515@gmail.com>
2023-09-15 07:17:51 -0700

    add assignment

---
+++ app.js
@@ -0,0 +1,97 @@
+// var arr = [];
+
+// arr [0] = "abc";
+// arr [1] = 12 ;
+// arr [2] = "ulfat";
+// arr [3] = "true"; 
+// console.log(arr);
+
+
+// var students=["ulfat",true,12,"yousuf",undefined];
+// students.pop();
+// students.pop();
+// students.push("aseela","asfiya")
+// console.log(students);
+
+
+// var country=["pakistan","lahore","islamabad"];
+// country.shift();
+// country.unshift("Karachi");
+// console.log(country);
+
+
+
+// var vegetables=["onion","potato","tomato"];
+// vegetables.splice(0,2,"cucumber");
+// console.log(vegetables);
+
+
+// var fruits=["orange","banana","grapes"];
+// var a=(0,3);
+// console.log(a);
+// **********************************************class task********************************************
+
+// Q1
+
+// var studentsname = [];
+// arr [0] = "ulfat";
+// arr [1] = "aseela" ;
+// arr [2] = "asfiya";
+// arr [3] = "bisma"; 
+// console.log(studentsname);
+
+
+
+// Q2
+ 
+// var arr =[];
+// arr [0] = "apple";
+// arr [1] = 12 ;
+// arr [2] = "ulfat";
+// arr [3] = "true"; 
+// console.log(arr);
+// Q3
+
+// var fruits = ["apple","mango","banana","orange"];
+// console.log(fruits);
+// Q4
+
+// var arr = [23];
+// console.log(arr)
+
+// Q5
+
+// var arr =[true,false];
+// console.log(arr)
+
+// // Q6
+
+// var arr = [true,"khatija",23];
+// console.log(arr)
+
+// Q7
+ 
+
+// var qualification=["SSC","HSC","BSC","BS","BCOM","MS","MPhil","PHD"];
+// document.write("Qualification:<br>" + "1." +  qualification[0]+ "<br>"+ qualification[1]+"<br>"+ qualification[2]+"<br>"+ qualification[3]+"<br>"+ qualification[4]+"<br>"+ qualification[5]+"<br>"+ qualification[6]+"<br>"+ qualification[7]+"<br>"
+//     )
+
+
+// Q8
+
+var students =["Michael","john","Tony"];
+
+var score=[320 , 230 , 480];
+var totalscore = 500;
+var percentage1=(score[0]/totalscore)*100;
+
+console.log(percentage1);
+
+var percentage2=(score[1]/totalscore)*100;
+
+console.log(percentage2);
+var percentage3=(score[2]/totalscore)*100;
+
+console.log(percentage3);
+
+document.write("score of   "+ students[1]+ percentage1)
